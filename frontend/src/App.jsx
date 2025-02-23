9822e7b03d2bb29ac0d0881c8e147763eccbd751
Manjeetmathur <manjeetkumar62054@gmail.com>
2025-02-24 03:43:51 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,34 +55,7 @@ function App() {
         </div>
       </div>
 
-      <div className="bg-gray-800 border-t-2 border-gray-200 px-4 pt-4 py-3">
-        <form onSubmit={review} className="relative flex">
-          <input
-            type="text"
-            value={code}
-            onChange={(e) => setCode(e.target.value)}
-            placeholder="Type a message..."
-            className="w-full focus:outline-none focus:placeholder-gray-400 text-white placeholder-gray-600 pl-4 bg-gray-950 rounded-md py-3"
-          />
-          <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
-            <button
-              type="submit"
-              className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
-            >
-              <span className="font-bold">Send</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-6 w-6 ml-2 transform rotate-90"
-              >
-                <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
-              </svg>
-            </button>
-          </div>
-        </form>
-      </div>
-
+     
       {/* Chat Messages */}
       <div className="flex-1 flex flex-col-reverse overflow-y-auto p-4 space-y-6 space-y-reverse bg-gray-950">
         {codes.map((message, idx) => (
@@ -119,6 +92,33 @@ function App() {
       </div>
 
       {/* Chat Input */}
+      <div className="bg-gray-800 border-t-2 border-gray-200 px-4 pt-4 py-3">
+        <form onSubmit={review} className="relative flex">
+          <input
+            type="text"
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+            placeholder="Type a message..."
+            className="w-full focus:outline-none focus:placeholder-gray-400 text-white placeholder-gray-600 pl-4 bg-gray-950 rounded-md py-3"
+          />
+          <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
+            <button
+              type="submit"
+              className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
+            >
+              <span className="font-bold">Send</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-6 w-6 ml-2 transform rotate-90"
+              >
+                <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+              </svg>
+            </button>
+          </div>
+        </form>
+      </div>
 
     </div>
 
